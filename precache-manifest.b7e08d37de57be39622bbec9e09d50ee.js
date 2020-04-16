8840709c96c138b76fcf443b722938526ef866b5
Nick Karkut <nk.karkut@gmail.com>
2020-04-16 16:45:55 -0400

    Updates

---
+++ precache-manifest.b7e08d37de57be39622bbec9e09d50ee.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79311699ef1e37b6a87fcb3db36c15cb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "10002d975c9259c66d2d",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b659afc78a1656d747f3",
+    "url": "/static/js/2.cf4207a6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.cf4207a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10002d975c9259c66d2d",
+    "url": "/static/js/main.0442cfd9.chunk.js"
+  },
+  {
+    "revision": "e2aeaeaaa57405442c33",
+    "url": "/static/js/runtime-main.cd8d6107.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
